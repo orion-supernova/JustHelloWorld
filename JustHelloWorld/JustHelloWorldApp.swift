9cc5070a6d9e18f1f938d69f3a5802b9f599ea42
Murat Can Koç <muratcankoc@gmail.com>
2022-02-07 20:58:17 +0300

    Initial Commit

---
+++ JustHelloWorld/JustHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustHelloWorldApp.swift
+//  JustHelloWorld
+//
+//  Created by Murat Can KOÇ on 7.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct JustHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
